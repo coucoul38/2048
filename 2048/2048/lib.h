73ebb913a0eb13b021c9629f99203372e56eccd5
Noham Coulon <44196664+coucoul38@users.noreply.github.com>
2023-10-23 15:54:41 +0200

    debug file is missing ?

---
+++ 2048/2048/lib.h
@@ -0,0 +1,31 @@
+
+
+class Grid {
+	public:
+		int size_x;
+		int size_y;
+		int** grid = {}
+
+		Grid() {
+			for (size_t i = 0; i < size_x; i++)
+			{
+				int col = {}
+				for (size_t z = 0; z < size_y; z++)
+				{
+					col[z] = 0;
+				}
+				grid[i] = col;
+			}
+		}
+
+		void print() {
+			for (size_t col = 0; col < size_x; col++)
+			{
+				for (size_t row = 0; row < size_y; row++) {
+					std::cout << char(this[col][row]);
+				}
+				std::cout << "\n";
+			}
+		}
+}
+
