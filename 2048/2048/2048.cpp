570682b4ae70ce280b5907de81c7325767d320f6
Noham Coulon <44196664+coucoul38@users.noreply.github.com>
2023-10-24 14:45:53 +0200

    input

--- 2048/2048/2048.cpp
+++ 2048/2048/2048.cpp
@@ -4,11 +4,50 @@
 #include <iostream>
 #include "lib.h"
 
+
+char readDirection(char direction) {
+
+    if (direction == 'H' || direction == 'h') {
+        std::cout << "Vers le haut";
+        return 'h';
+    }
+    else if (direction == 'B' || direction == 'b') {
+        std::cout << "Vers le bas";
+        return 'b';
+    }
+    else if (direction == 'G' || direction == 'g') {
+        std::cout << "Vers la gauche\n";
+        return 'g';
+    }
+    else if (direction == 'D' || direction == 'd') {
+        std::cout << "Vers la droite\n";
+        return 'd';
+    }
+    else if (direction == 'E' || direction == 'e') {
+        std::cout << "Exiting loop\n";
+        return 'e';
+    }
+}
+
+
 int main()
 {
+    bool exit = false;
     std::cout << "Hello World!\n";
     Grid* grid = new Grid(4,4);
-    grid->print();
+
+    char player_input;
+    int key;
+
+    while(!exit) {
+        grid->print();
+        std::cout << "B/H/D/G pour deplacer ou E pour sortir: ";
+        std::cin >> player_input;
+        key = readDirection(player_input);
+        if (key == 'e') {
+            exit = true;
+        }
+    }
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
